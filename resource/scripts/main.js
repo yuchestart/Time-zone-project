9251b825a4c684deb95fdeb8a7fb44ead8567b64
What-ever-this-is <yuchestart@gmail.com>
2023-06-20 15:06:34 +0800

    :trollface::trollface::trollface::trollface:

--- resource/scripts/main.js
+++ resource/scripts/main.js
@@ -9,6 +9,67 @@ function $(x,parent){
     }
 }
 function main(){
-
+    new Time()
 }
 window.onload = main;
+class Time{
+    /**
+     * String:
+     * DD-MM-YYYY-HH-MM-SS-HH-MM
+     * Numbers:
+     * HH,MM,SS,dd,mm,yy,hh,mm
+     * @param  {...String|Number} theargs 
+     */
+    constructor(...theargs){
+        if(theargs.length == 1){
+            var time = theargs[0].split("-");
+            this.date = parseInt(time[0]);
+            this.month = parseInt(time[1]);
+            this.year = parseInt(time[2]);
+            this.hour = parseInt(time[3]);
+            this.minute = parseInt(time[4]);
+            this.second = parseInt(time[5]);
+            this.timezoneoffset = {
+                hours:parseInt(time[6]),
+                minute:parseInt(time[7])
+            };
+        } else if(theargs.length == 7){
+            this.hour = theargs[0]
+            this.minute = theargs[1]
+            this.second = theargs[2]
+            this.date = theargs[3]
+            this.month = theargs[4]
+            this.year = theargs[5]
+            this.timezoneoffset = {
+                hours:theargs[6],
+                minute:theargs[7]
+            }
+        }
+    }
+    settime(...theargs){
+        if(theargs.length == 1){
+            var time = theargs[0].split("-");
+            this.date = parseInt(time[0]);
+            this.month = parseInt(time[1]);
+            this.year = parseInt(time[2]);
+            this.hour = parseInt(time[3]);
+            this.minute = parseInt(time[4]);
+            this.second = parseInt(time[5]);
+            this.timezoneoffset = {
+                hours:parseInt(time[6]),
+                minute:parseInt(time[7])
+            };
+        } else if(theargs.length == 7){
+            this.hour = theargs[0]
+            this.minute = theargs[1]
+            this.second = theargs[2]
+            this.date = theargs[3]
+            this.month = theargs[4]
+            this.year = theargs[5]
+            this.timezoneoffset = {
+                hours:theargs[6],
+                minute:theargs[7]
+            }
+        }
+    }
+}
--- resource/scripts/table.js
+++ resource/scripts/table.js
@@ -4,6 +4,7 @@ class TableElement{
         if(islocaltime){
             mydate = new Date();
             this.utcdifference = (mydate.getHours()+timedifference) - mydate.getUTCHours();
+            this.utcminutedifference
         } else {
             this.utcdifference = timedifference;
         }
@@ -14,13 +15,14 @@ class TableElement{
      * @param {Date} time 
      */
     getTime(time){
-        var newhours = time.getHours()+this.utcdifference();
+        var newhours = time.getUTCHours()+this.utcdifference();
         var mydate = new Date()
         if(newhours > 23){
             newhours-=23
         }
         mydate.setHours(newhours);
-        mydate.setMinutes(time.getMinutes());
+        mydate.set
+        mydate.setMinutes(time.getUTCMinutes());
         mydate.setSeconds(time.getSeconds());
         return newhours
     }
